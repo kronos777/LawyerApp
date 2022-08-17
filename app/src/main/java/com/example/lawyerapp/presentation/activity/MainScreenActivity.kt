dddf456bfa75da15a87be827f741ecbc21a047f6
Kronos777 <ziswp1@gmail.com>
2022-08-17 19:12:11 +0300

    add main user screen 0.8

--- app/src/main/java/com/example/lawyerapp/presentation/activity/MainScreenActivity.kt
+++ app/src/main/java/com/example/lawyerapp/presentation/activity/MainScreenActivity.kt
@@ -5,10 +5,12 @@ import android.os.Bundle
 import android.view.MenuItem
 import android.widget.Toast
 import androidx.appcompat.app.ActionBarDrawerToggle
+import androidx.appcompat.widget.Toolbar
 import androidx.drawerlayout.widget.DrawerLayout
 import com.example.lawyerapp.R
 import com.example.lawyerapp.databinding.ActivityMainBinding
 import com.example.lawyerapp.databinding.ActivityMainScreenBinding
+import com.google.android.material.appbar.MaterialToolbar
 
 class MainScreenActivity : AppCompatActivity() {
 
@@ -23,16 +25,57 @@ class MainScreenActivity : AppCompatActivity() {
         binding = ActivityMainScreenBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
-        (this as AppCompatActivity).supportActionBar?.title = "Судный день"
+       // (this as AppCompatActivity).supportActionBar?.setCustomView(binding.appBarTop)
+        //(this as AppCompatActivity).supportActionBar?.title = "Судный день"
+
+        //(this as MainScreenActivity).enableHomeBackIcon(false)
+        /*test*/
+       /* val appBarConfiguration = AppBarConfiguration(
+            setOf(
+                R.id.nav_home, R.id.nav_slideshow
+            ), drawerLayout
+        )
+
+        setupActionBarWithNavController(navController, appBarConfiguration)
+*/
+
+        // create instance of the material toolbar
+        val materialToolbar: MaterialToolbar = binding.toolBar
+
+        // assign the on menu item click listener
+        materialToolbar.setOnMenuItemClickListener {
+            when (it.itemId) {
+                R.id.favorite -> {
+                    Toast.makeText(this, "Favorites Clicked", Toast.LENGTH_SHORT).show()
+                    true
+                }
+                R.id.search -> {
+                    Toast.makeText(this, "Search Clicked", Toast.LENGTH_SHORT).show()
+                    true
+                }
+                else -> false
+            }
+        }
 
 
-        val drawerLayout: DrawerLayout? = binding.drawerLayoutId
+        toggle = getActionBarDrawerToggle(binding.drawerLayoutId, binding.toolBar).apply {
+            setToolbarNavigationClickListener {
+                // Back to home fragment for any hit to the back button
+                 //  navController.navigate(R.id.app_bar_top)
+            }
+            // Intialize the icon at the app start
+            enableHomeBackIcon(false)
+        }
+        /*test*/
+
+
+     /*   val drawerLayout: DrawerLayout? = binding.drawerLayoutId
 
         toggle = ActionBarDrawerToggle(this, drawerLayout, R.string.open, R.string.close)
         drawerLayout?.addDrawerListener(toggle)
         toggle.syncState()
         supportActionBar?.setDisplayHomeAsUpEnabled(true)
-
+*/
 
         binding.navView.setNavigationItemSelectedListener {
 
@@ -61,5 +104,31 @@ class MainScreenActivity : AppCompatActivity() {
         return super.onOptionsItemSelected(item)
     }
 
+    fun enableHomeBackIcon(enabled: Boolean) {
+        // Enable/Disable opening the drawer from the start side
+        toggle?.isDrawerIndicatorEnabled = !enabled
+
+        // Change the default burger icon
+        supportActionBar?.setHomeAsUpIndicator(
+            if (enabled) R.drawable.next
+            else R.drawable.ic_baseline_menu_24
+        )
+    }
+
+    private fun getActionBarDrawerToggle(
+        drawerLayout: DrawerLayout,
+        toolbar: Toolbar
+    ): ActionBarDrawerToggle {
+        toggle = ActionBarDrawerToggle(
+            this,
+            drawerLayout,
+            toolbar,
+            R.string.open,
+            R.string.close
+        )
+        drawerLayout.addDrawerListener(toggle!!)
+        toggle?.syncState()
+        return toggle as ActionBarDrawerToggle
+    }
 
 }
